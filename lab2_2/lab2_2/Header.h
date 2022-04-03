2d823c16b6049a5a528ca3326e48857f6755baf0
maksloposha <maksloposha@gmail.com>
2022-04-03 13:57:10 +0300

    Create Header.h

---
+++ lab2_2/lab2_2/Header.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+using namespace std;
+void Input(string fileName);
+int text_mod(string FileNameIn);
+int output(string fileName);  
+void last_bus(string fileName);
+void add(string fileName);
