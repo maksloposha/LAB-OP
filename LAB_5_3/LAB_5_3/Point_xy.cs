c278c2b474f6c2d6f87ee92bbbdd9b99930b7ef3
maksloposha <maksloposha@gmail.com>
2022-05-24 01:43:13 +0300

    Create Point_xy.cs

---
+++ LAB_5_3/LAB_5_3/Point_xy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LAB_5_3
+{
+    public class Point_xy : Point
+    {
+        private int a1, a2;
+        private int x0, y0;
+        private int x, y;
+        public Point_xy()
+        {
+            Random rand = new Random();
+            a1 = rand.Next(-10, 10);
+            a2 = rand.Next(-10, 10);
+            x0 = rand.Next(-10, 10);
+            y0 = rand.Next(-10, 10);
+            x = 0;
+            y = 0;
+        }
+        public override int getX() => x;
+        public override int getY() => y;
+        public override int getZ() => 0;
+        public override double Destination<T>(T point)
+        {
+            return Math.Sqrt(Math.Pow(point.getX() - x, 2) + Math.Pow(point.getY() - y, 2));
+        }
+        public override void koorPoint(int t)
+        {
+            x = x0 + Convert.ToInt32(a1 * Math.Sin(t));
+            y = y0 + Convert.ToInt32(a2 * Math.Cos(t));
+        }
+    }
+}
+
+
