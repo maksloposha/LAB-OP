776e0d453dd95b49ea4a0c6707ff37e6c2f7fae0
maksloposha <maksloposha@gmail.com>
2022-05-24 01:43:10 +0300

    Create Point.cs

---
+++ LAB_5_3/LAB_5_3/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace LAB_5_3
+{
+    public abstract class Point
+    {
+        public abstract int getX();
+        public abstract int getY();
+        public abstract int getZ();
+        public abstract void koorPoint(int t);
+        public abstract double Destination<T>(T point) where T : Point;
+
+    }
+}
+
+
